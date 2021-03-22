0a28878a382274847e91dd69dce465f9ea7eb1d8
Jonathan Hamer <jonathanhamer@Jonathans-Air.attlocal.net>
2021-03-22 18:12:43 -0400

    create front end react

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Create the Server.js file for the backend and install express
     install express, dotenv, body-parser, mongoose 
 
 Add basic funcionality to the server.js file 
+
+Create react app in the client folder to have a front end set up 
+
+Change server port / and package.json to run concurrently
+
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+
 const app = express();
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI); //mongodb://localhost/loginapp
@@ -21,7 +23,7 @@ app.get('/', (req,res) => {
   res.send('Hello world!')
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Magic happening on port " + PORT);
 })
