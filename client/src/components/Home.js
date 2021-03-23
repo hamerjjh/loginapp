bf8d3e3dd048889a20649b781a797365637af895
Jonathan Hamer <jonathanhamer@Jonathans-Air.attlocal.net>
2021-03-23 11:59:44 -0400

    created client side homepage

---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the leader in digital content management</h1>
+        <h2>Please Register or Sign in Below!</h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
